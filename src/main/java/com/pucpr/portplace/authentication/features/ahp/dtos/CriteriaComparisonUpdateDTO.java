f611b1c8d5acded1a3112ece080112625193be89
Gabriel <gabrielgmns071204@gmail.com>
2025-05-24 16:32:01 -0300

    chore: removido imports nao utilizados

--- src/main/java/com/pucpr/portplace/authentication/features/ahp/dtos/CriteriaComparisonUpdateDTO.java
+++ src/main/java/com/pucpr/portplace/authentication/features/ahp/dtos/CriteriaComparisonUpdateDTO.java
@@ -2,9 +2,7 @@ package com.pucpr.portplace.authentication.features.ahp.dtos;
 
 import com.pucpr.portplace.authentication.features.ahp.enums.ImportanceScale;
 
-import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
-import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
