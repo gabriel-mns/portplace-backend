ac621f07e89bef3c4063d072c11dd5df5ca9a37d
Gabriel <gabrielgmns071204@gmail.com>
2025-09-26 20:08:47 -0300

    fix: removido campos nao utilizados de UPDATE de risco

--- src/main/java/com/pucpr/portplace/features/portfolio/dtos/risk/RiskUpdateDTO.java
+++ src/main/java/com/pucpr/portplace/features/portfolio/dtos/risk/RiskUpdateDTO.java
@@ -1,8 +1,5 @@
 package com.pucpr.portplace.features.portfolio.dtos.risk;
 
-import java.time.LocalDateTime;
-
-import com.fasterxml.jackson.annotation.JsonFormat;
 import com.pucpr.portplace.core.validation.constraints.enumValues.ValidEnum;
 import com.pucpr.portplace.features.portfolio.enums.RiskScaleEnum;
 
@@ -27,20 +24,5 @@ public class RiskUpdateDTO {
     private String impactDescription;
     private String preventionPlan;
     private String contingencyPlan;
-
-    // Calculated Fields
-    private Integer severity;
-
-    // Relationships
-
-    // Auditing Fields
-    private boolean disabled;
-    
-    @JsonFormat(pattern = "dd/mm/yyyy HH:mm:ss")
-    private LocalDateTime createdAt;
-    @JsonFormat(pattern = "dd/mm/yyyy HH:mm:ss")
-    private LocalDateTime lastModifiedAt;
-    private String createdBy;
-    private String lastModifiedBy;
 
 }
